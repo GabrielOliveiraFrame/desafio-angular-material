a060008cb565d591c58865818d83f1a1485ea7bb
Gabriel Oliveira <gabrieloliveira@frwk.com.br>
2023-10-06 13:34:06 -0300

    Setting prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API_TODOS_URL: 'https://jsonplaceholder.typicode.com/todos',
+  API_POSTS_URL: 'https://jsonplaceholder.typicode.com/posts',
+  API_COMMENTS_URL: 'https://jsonplaceholder.typicode.com/comments',
+  API_ALBUMS_URL: 'https://jsonplaceholder.typicode.com/albums'
 };
